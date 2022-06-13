19fbf4e6d2a3d4e36789eaf2de3ff0098d534851
nontakorn223 <69671412+nontakorn223@users.noreply.github.com>
2022-06-13 23:53:22 +0700

    fetchTest

---
+++ jobbee/lib/model.dart/job.dart
@@ -0,0 +1,9 @@
+class Job {
+  final String name;
+  final String id;
+  final String companyName;
+  final String images;
+
+  Job({ required this.name, required this.id, required this.companyName, required this.images});
+
+}
